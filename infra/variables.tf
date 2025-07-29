554a2b363422c0469228c2aa02b21a7e4c208a2a
root <root@tejas.localdomain>
2025-07-29 19:59:02 +0000

    variables.tf update

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,24 +1,10 @@
-variable "ec2_instance_type"{
-    default = "t2.micro"
-    type = string
+variable "aws_access_key" {
+  description = "AWS Access Key ID"
+  type        = string
 }
 
-variable "ec2_instance_volume"{
-    default = 8
-    type = number
+variable "aws_secret_key" {
+  description = "AWS Secret Access Key"
+  type        = string
 }
 
-variable "ec2_instance_image"{
-    default = "ami-0f918f7e67a3323f0"
-    type = string
-}
-
-variable "ec2_instance_volume_type" {
-    default = "gp3"
-    type = string
-}
-
-variable "region" {
-    default = "ap-south-1"
-    type = string
-}
