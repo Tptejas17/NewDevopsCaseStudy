eee774667c1783edf2bf2938918f468cec307a2f
root <root@tejas.localdomain>
2025-07-30 10:52:12 +0000

    Added HTTP (port 80) rule to security group for browser access

--- infra/main.tf
+++ infra/main.tf
@@ -18,6 +18,15 @@ resource "aws_security_group" "ssh_access" {
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow SSH"
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow HTTP"
   }
 
   egress {
