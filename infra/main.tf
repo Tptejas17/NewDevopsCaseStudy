38e7efcb091f14f56e310210d06d23e6af75d8cc
Tptejas17 <tejasparabinvest113@gmail.com>
2025-09-01 11:19:35 +0530

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -9,7 +9,7 @@ data "aws_vpc" "default" {
 }
 
 resource "aws_security_group" "ssh_access" {
-  name_prefix = "allow_ssh_jenkins_"
+  name_prefix = "allow_ssh_jenkins1"
   description = "Allow SSH access from anywhere"
   vpc_id      = data.aws_vpc.default.id
 
